ae2fe4be06ce2e37b0b5ee5b9438e0b68abd8cfe
Akshaya Parida <akshayaparida@example.com>
2025-12-11 18:14:29 +0530

    fix: update Jest config to exclude utility files from test matching

--- jest.config.js
+++ jest.config.js
@@ -14,6 +14,10 @@ const customJestConfig = {
     '**/__tests__/**/*.[jt]s?(x)',
     '**/?(*.)+(spec|test).[jt]s?(x)'
   ],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    'src/__tests__/utils/',
+  ],
   collectCoverageFrom: [
     'src/**/*.{js,jsx,ts,tsx}',
     '!src/**/*.d.ts',
